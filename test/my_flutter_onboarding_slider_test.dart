364e8e0bfb3e517359bbcd2d171a66091c52e70f
MuhammadAliRaxa <muhammadaliraza7427@gmail.com>
2025-10-30 13:54:00 +0500

    First

---
+++ test/my_flutter_onboarding_slider_test.dart
@@ -0,0 +1,16 @@
+import 'package:my_flutter_onboarding_slider/my_flutter_onboarding_slider.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
